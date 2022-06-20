2acdcea70766d6f88659f244210946160175ab8e
samy884 <ssgill884@gmail.com>
2022-06-20 23:06:31 +0530

    configuring cartReducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./store-slices/cart-slice";
 
 const store = configureStore({
-    reducer: {}
+  reducer: { cart: cartReducer },
 });
 
 export default store;
