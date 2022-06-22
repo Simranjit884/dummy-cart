4927583b2f60d7f80a7d63bb1a6e556d6289bee1
samy884 <ssgill884@gmail.com>
2022-06-22 15:26:02 +0530

    created removeItems

--- src/store/store-slices/cart-slice.js
+++ src/store/store-slices/cart-slice.js
@@ -1,13 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-createSlice({
+const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
     totalQuantity: 0,
   },
   reducers: {
-    addItemsToCart(action, state) {
+    addItemsToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       if (existingItem) {
@@ -22,6 +22,22 @@ createSlice({
         existingItem.quantity++;
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
+      state.totalQuantity++;
     },
+    removeItemsFromCart(state, action) {
+        const itemId = action.payload;
+        const existingItem = state.items.find(item=> item.id === itemId);
+        if(existingItem.quantity === 1) {
+            const updatedCartItems = state.items.filter(item=> item.id !== existingItem.id);
+            state.items = updatedCartItems;
+        } else {
+            existingItem.quantity--;
+            existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+        }
+        existingItem.totalQuantity--;
+    }
   },
 });
+
+export const cartActions = cartSlice.actions;
+export default cartSlice.reducer;
