4b2626697a31bcc20a23e06186b1769c9d615596
samy884 <ssgill884@gmail.com>
2022-06-22 15:28:32 +0530

    created product items

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,5 +1,5 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
 
 const Products = (props) => {
   return (
@@ -7,9 +7,44 @@ const Products = (props) => {
       <h2>Buy your favorite products</h2>
       <ul>
         <ProductItem
-          title='Test'
+          title="Test1"
           price={6}
-          description='This is a first product - amazing!'
+          description="This is a first product - amazing!"
+        />
+      </ul>
+      <ul>
+        <ProductItem
+          title="Test2"
+          price={6}
+          description="This is a second product - amazing!"
+        />
+      </ul>
+      <ul>
+        <ProductItem
+          title="Test3"
+          price={6}
+          description="This is a third product - amazing!"
+        />
+      </ul>
+      <ul>
+        <ProductItem
+          title="Test4"
+          price={6}
+          description="This is a fourth product - amazing!"
+        />
+      </ul>
+      <ul>
+        <ProductItem
+          title="Test5"
+          price={6}
+          description="This is a fifth product - amazing!"
+        />
+      </ul>
+      <ul>
+        <ProductItem
+          title="Test6"
+          price={6}
+          description="This is a sixth product - amazing!"
         />
       </ul>
     </section>
